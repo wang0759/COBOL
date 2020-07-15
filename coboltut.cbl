550088d741ca638e8a15adef44d0ab096b768e7d
Quanyi Wang <wang0759@algonquinlive.com>
2020-07-15 18:38:02 -0400

    DATA TYPE PRACTICES

--- coboltut.cbl
+++ coboltut.cbl
@@ -10,40 +10,35 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       01 UserName PIC X(30) VALUE "YOU".
-       01 Num1 PIC 9 VALUE ZEROS.
-       01 Num2 PIC 9 VALUE ZEROS.
-       01 Total PIC 99 VALUE 0.
-       01 SSNum.
-           02 SSArea PIC 999.
-           02 SSGroup PIC 99.
-           02 SSSerial PIC 9999.
-        01 PIValue CONSTANT AS 3.14.
-
-        *> ZERO, ZEROES
-        *> SPACE SPACES    figurative constances examples
-        *> HIGH-VALUES
-        *> LOW-VALUES
+       01 SampleData PIC X(10) VALUE "Stuff".
+       01 JustLetters PIC AAA VALUE "ABC".
+       01 JustNums PIC 9(4) VALUE 1234.
+       01 SignedInt PIC S9(4) VALUE -1234.
+       *> V99: 2 DECIMAL PLACES   
+       01 PayCheck PIC 9(4)V99 VALUE ZEROS.
+       01 Customer.
+           02 Ident PIC 9(3).
+           02 CustName PIC X(12).
+           02 DateOfBirth.
+               03 MOB PIC 99.
+               03 DOB PIC 99.
+               03 YOB PIC 9(4).
+       01 Num1 PIC 9 VALUE 5.
+       01 Num2 PIC 9 VALUE 4.
+       01 Num3 PIC 9 VALUE 3.
+       01 Ans PIC S99V99 VALUE 0.
+        *>    SIGNED 2 DIGITS WITH 2 DECIMALS
+       01 Rem PIC 9V99.
 
        PROCEDURE DIVISION.
-       DISPLAY "What is your name? " WITH NO ADVANCING
-       ACCEPT userName
-       DISPLAY "HELLO " userName
-
-       MOVE ZERO TO UserName
-       DISPLAY UserName
-
-       DISPLAY "PLEASE ENTER 2 NUMBERS FOR TOTAL"
-       ACCEPT Num1
-       ACCEPT Num2
-       COMPUTE Total = Num1 + Num2
-       DISPLAY Num1 "+" Num2 "=" Total
-
-       DISPLAY "ENTER YOUR SSN, PLZ"
-       ACCEPT SSNum
-       DISPLAY "AREA " SSArea
-       display "group " SSGroup
-       DISPLAY "SERIAL " SSSerial
+       MOVE "MORE STUFF" TO SampleData
+       MOVE "123" TO SampleData
+       MOVE 123 TO SampleData
+       DISPLAY SampleData
+       DISPLAY PayCheck
+       MOVE "123Wang Quanyi 12251990" to Customer
+       DISPLAY CustName
+       DISPLAY MOB "/" DOB "/" YOB
 
        STOP RUN.
 
