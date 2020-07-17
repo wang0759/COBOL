d085e6df30312a2845fdb91259c732c3179971a2
Quanyi Wang <wang0759@algonquinlive.com>
2020-07-17 18:01:32 -0400

    menu system which allows you to Add, Update, Delete and Display Customer Data

--- coboltut.cbl
+++ coboltut.cbl
@@ -1,96 +1,133 @@
-       >>SOURCE FORMAT FREE
+       >>SOURCE FORMAT FREE       
 IDENTIFICATION DIVISION.
-PROGRAM-ID. COBOLTUT.
-*> Here we'll design and print a customer report
- 
+PROGRAM-ID. tutorial10.
+*> This program has a menu system and allows you to
+*> Add, Update, Delete and Display Customer Data
 ENVIRONMENT DIVISION.
 INPUT-OUTPUT SECTION.
 FILE-CONTROL.
-       *> Define the file to save the report to
-       SELECT CustomerReport ASSIGN TO "CustReport.rpt"
-           ORGANIZATION IS LINE SEQUENTIAL.
-       *> The file that provides the data
-       SELECT CustomerFile ASSIGN TO "Customer.dat"
-           ORGANIZATION IS LINE SEQUENTIAL.
+*> Select to use a file with keys (Indexed File)
+*> We will randomly access data vs. sequential
+*> Define the name associated with the key
+       SELECT CustomerFile ASSIGN TO "customers.txt"
+           ORGANIZATION IS INDEXED
+           ACCESS MODE IS RANDOM
+           RECORD KEY IS IDNum.
  
 DATA DIVISION.
 FILE SECTION.
-*> Define FD and custom print line
-FD CustomerReport.
-01 PrintLine PIC X(44).
- 
-*> Info on customer data
+*> Model customer data
 FD CustomerFile.
-01 CustomerData.
-       02 IDNum    PIC 9(8).
-       02 CustName.
-           03 FirstName    PIC X(15).
-           03 LastName     PIC X(15).
-       88 WSEOF   VALUE HIGH-VALUE.
+       01 CustomerData.
+           02 IDNum PIC 99.
+           02 FirstName PIC X(15).
+           02 LastName PIC X(15).
  
 WORKING-STORAGE SECTION.
-*> Break the report up into pieces
-01 PageHeading.
-       02 FILLER PIC X(13) VALUE "Customer List".
-01 PageFooting.
-       02 FILLER PIC X(15) VALUE SPACE.
-       02 FILLER PIC X(7) VALUE "Page : ".
-       02 PrnPageNum PIC Z9.
-*> Column headings for data
-01 Heads PIC X(40) VALUE " IDNum       FirstName        LastName".
-*> Customer data to print with spaces defined
-01 CustomerDetailLine.
-       02 FILLER PIC X VALUE SPACE.
-       02 PrnCustID PIC 9(8).
-       02 FILLER PIC X(4) VALUE SPACE.
-       02 PrnFirstName PIC X(15). 
-       02 FILLER PIC XX VALUE SPACE.
-       02 PrnLastName PIC X(15).
-*> Printed at end of report
-01 ReportFooting PIC X(13) VALUE "END OF REPORT".
-*> Tracks number of lines used, when to print footer
-*> and new heading
-01 LineCount PIC 99 VALUE ZERO.
-       88 NewPageRequired VALUE 40 THRU 99.
-*> Track number of pages
-01 PageCount PIC 99 VALUE ZERO.
+       *> Customer menu choice
+       01 Choice PIC 9.
+       *> Tracks whether to exit
+       01 StayOpen PIC X VALUE 'Y'.
+       *> Tracks whether the customer exists
+       01 CustExists PIC X.
  
 PROCEDURE DIVISION.
-PrintReport.
-OPEN INPUT CustomerFile
-OPEN OUTPUT CustomerReport
-PERFORM PrintPageHeading
-*> Read customer file until end
-READ CustomerFile
-       AT END SET WSEOF TO TRUE
-END-READ
-PERFORM PrintReportBody UNTIL WSEOF
-*> Advancing moves down defined number of lines
-WRITE PrintLine FROM ReportFooting AFTER ADVANCING 5 LINES
-CLOSE CustomerFile, CustomerReport
-STOP RUN.
+StartPara.
+       *> To access data randomly you must use I-O mode
+       OPEN I-O CustomerFile.
+       *> Continue execution until StayOpen is N which
+       *> happens if the user enters a number not 1 thru 4
+       PERFORM UNTIL StayOpen='N'
+           DISPLAY " "
+           DISPLAY "CUSTOMER RECORDS"
+           DISPLAY "1 : Add Customer"
+           DISPLAY "2 : Delete Customer"
+           DISPLAY "3 : Update Customer"
+           DISPLAY "4 : Get Customer"
+           DISPLAY "0 : Quit"
+           DISPLAY ": " WITH NO ADVANCING
+           ACCEPT Choice
+           *> Execute different paragraphs based on option
+           EVALUATE Choice
+               WHEN 1 PERFORM AddCust
+               WHEN 2 PERFORM DeleteCust
+               WHEN 3 PERFORM UpdateCust
+               WHEN 4 PERFORM GetCust
+               *> When N we jump out of the loop
+               WHEN OTHER move 'N' TO StayOpen
+           END-EVALUATE
+          
+       END-PERFORM.
+       *> Close the file and stop execution
+       CLOSE CustomerFile
+       STOP RUN.
  
-*> Prints heading and tracks page count
-PrintPageHeading.
-WRITE PrintLine FROM PageHeading AFTER ADVANCING Page
-WRITE PrintLine FROM Heads AFTER ADVANCING 5 LINES
-MOVE 3 TO LineCount
-ADD 1 TO PageCount.
+AddCust.
+       DISPLAY " ".
+       DISPLAY "Enter ID : " WITH NO ADVANCING.
+       ACCEPT IDNum.
+       DISPLAY "Enter First Name : "  WITH NO ADVANCING.
+       ACCEPT FirstName.
+       DISPLAY "Enter Last Name : " WITH NO ADVANCING.
+       ACCEPT LastName.
+       DISPLAY " ".
+       *> Write customer data or display error if ID taken
+       WRITE CustomerData
+           INVALID KEY DISPLAY "ID Taken"
+       END-WRITE.
  
-*> Handles creating new page logic and printing customer
-*> data
-PrintReportBody.
-IF NewPageRequired
-       MOVE PageCount TO PrnPageNum
-       WRITE PrintLine FROM PageFooting AFTER ADVANCING 5 LINES
-       PERFORM PrintPageHeading
-END-IF
-*> Move data to be printed to report
-MOVE IDNum TO PrnCustID
-MOVE FirstName TO PrnFirstName
-MOVE LastName TO PrnLastName
-WRITE PrintLine FROM CustomerDetailLine AFTER ADVANCING 1 LINE
-ADD 1 TO LineCount
-READ CustomerFile
-       AT END SET WSEOF TO TRUE
-END-READ.
+ 
+DeleteCust.
+       DISPLAY " ".
+       DISPLAY "Enter Customer ID to Delete : " WITH NO ADVANCING.
+       ACCEPT IDNum.
+       *> Delete customer based on ID
+       DELETE CustomerFile
+       INVALID KEY DISPLAY "Key Doesn't Exist"
+       END-DELETE.
+ 
+UpdateCust.
+       MOVE 'Y' TO CustExists.
+       DISPLAY " ".
+       DISPLAY "Enter ID to Update : " WITH NO ADVANCING.
+       ACCEPT IDNum.
+       *> Read customer or mark N if doesn't exist
+       READ CustomerFile
+           INVALID KEY MOVE 'N' TO CustExists
+       END-READ.
+       *> Display error because ID doesn't exist
+       IF CustExists='N'
+           DISPLAY "Customer Doesn't Exist"
+       ELSE
+           DISPLAY "Enter the New First Name : " WITH NO ADVANCING
+           ACCEPT FirstName
+           DISPLAY "Enter the New Last Name : " WITH NO ADVANCING
+           ACCEPT LastName
+       END-IF.
+       *> Update record for matching ID
+       REWRITE CustomerData
+           INVALID KEY DISPLAY "Customer Not Updated"
+       END-REWRITE.
+ 
+ 
+GetCust.
+       *> Assume customer exists
+       MOVE 'Y' TO CustExists.
+       DISPLAY " ".
+       DISPLAY "Enter Customer ID to Find : " WITH NO ADVANCING.
+       ACCEPT IDNum.
+       *> Mark N if customer ID doesn't exist
+       READ CustomerFile
+           INVALID KEY MOVE 'N' TO CustExists
+       END-READ.
+       *> Display error
+       IF CustExists='N'
+           DISPLAY "Customer Doesn't Exist"
+       ELSE
+           DISPLAY "ID : " IDNum
+           DISPLAY "First Name : " FirstName
+           DISPLAY "Last Name : " LastName
+END-IF.
+
+
+
