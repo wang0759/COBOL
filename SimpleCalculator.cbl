6846bf29bf9a4819f8fff3472fca82cb8f85ef33
Quanyi Wang <wang0759@algonquinlive.com>
2020-12-24 22:58:36 -0500

    calculatorUpdated

--- SimpleCalculator.cbl
+++ SimpleCalculator.cbl
@@ -17,31 +17,45 @@ WORKING-STORAGE SECTION.
 01  Num1         PIC 9  VALUE 7.
       *>  88 ValidNumber PIC 9  VALUE 0 THRU 9.
 01  Num2         PIC 9  VALUE 3.
-01  Result       PIC --9.99 VALUE 0.
+01  Result       PIC --9(5).99 VALUE 0.
 01  Operator     PIC X  VALUE "-".
-    88 ValidOperator   VALUES "*", "+", "-", "/".
+    88 ValidOperator   VALUES "*", "+", "-", "/", "s".
+    88 EndOfCalculate  VALUES "s".
 
 PROCEDURE DIVISION.
-CalculateResult.
-    DISPLAY "Enter a single digit number : " WITH NO ADVANCING
-    ACCEPT Num1
-   *>  IF Num1 IS NOT ValidNumber then
-   *>     display "INVALID NUMBER"
-   *>  END-IF.
-    DISPLAY "Enter a single digit number : " WITH NO ADVANCING
-    ACCEPT Num2
-    DISPLAY "Enter the operator to be applied : " WITH NO ADVANCING
-    ACCEPT Operator 
-    EVALUATE Operator
-      WHEN "+"   ADD Num2 TO Num1 GIVING Result
-      WHEN "-"   SUBTRACT Num2 FROM Num1 GIVING Result
-      WHEN "*"   MULTIPLY Num2 BY Num1 GIVING Result
-      WHEN "/"   DIVIDE Num1 BY Num2 GIVING Result ROUNDED
-      WHEN OTHER DISPLAY "Invalid operator entered"
-    END-EVALUATE
-    IF ValidOperator
-       DISPLAY "Result = ", Result
-    END-IF
+BEGIN.
+       PERFORM  GetValidOperator UNTIL ValidOperator
+       PERFORM UNTIL EndOfCalculate OR NOT  ValidOperator
+           PERFORM GetTwoNumbers
+    
+           EVALUATE Operator
+           WHEN "+"   ADD Num2 TO Num1 GIVING Result
+           WHEN "-"   SUBTRACT Num2 FROM Num1 GIVING Result
+           WHEN "*"   MULTIPLY Num2 BY Num1 GIVING Result
+           WHEN "/"   DIVIDE Num1 BY Num2 GIVING Result ROUNDED
+           WHEN OTHER DISPLAY "Invalid operator entered"
+           END-EVALUATE
+    
+           DISPLAY "Result = ", Result
+           MOVE SPACE TO Operator
+           PERFORM  GetValidOperator UNTIL ValidOperator
+           END-PERFORM
+       DISPLAY "END OF CALCULATIONS"
+    
 
 
 STOP RUN.
+
+GetValidOperator.
+       DISPLAY "Enter the operator to be applied (+ - * /) (s to end) : " WITH NO ADVANCING
+       ACCEPT Operator.
+
+GetTwoNumbers.
+       DISPLAY "Enter a single digit number : " WITH NO ADVANCING
+    ACCEPT Num1
+    DISPLAY "Enter a single digit number : " WITH NO ADVANCING
+    ACCEPT Num2.
+    
+       
+
+
